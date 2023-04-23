e0eb1e3958b7b3ef0ff6cecc2b1f4ac7c7c129bc
Zir Ammamiya <starouotw@gmail.com>
2023-04-23 17:21:13 +0800

    ver.0.0.5 branch test

--- lolicon.py
+++ lolicon.py
@@ -3,11 +3,9 @@
 #staging - get ready to commit but still doesn't. Use "git add (files)" from working / "git rm --cached (files)" back to working status
 #commit - git commit -m "(message)"
 import random
-age = random.randint(1,20)
+age = input("Enter the age : ")
 if age <= 12:
     print("loli")
 else:
     print("not loli")
 print(age)
-def func():
-    return
