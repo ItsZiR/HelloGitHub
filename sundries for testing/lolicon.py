388e1445e9ffedc1a99c97429a0c5327cf03e1b5
Zir Ammamiya <starouotw@gmail.com>
2023-04-29 15:20:00 +0800

    整理了一下資料夾

---
+++ sundries for testing/lolicon.py
@@ -0,0 +1,12 @@
+#3 environment:
+#working - not being added 
+#staging - get ready to commit but still doesn't. Use "git add (files)" from working / "git rm --cached (files)" back to working status
+#commit - git commit -m "(message)"
+import random
+age = input("Enter the age : ")
+if age <= 12:
+    print("loli")
+else:
+    print("not loli")
+print(age)
+#branch test
